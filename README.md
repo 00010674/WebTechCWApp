21937a3e7b0151c02a41225aad8754c681645cbb
Sardorbek Kamalov <kamalov184@gmail.com>
2021-04-07 17:05:41 +0500

    added readme, added new pages, added new buttons, many other changes

---
+++ README.md
@@ -0,0 +1,33 @@
+# Tasks Web Application
+
+### This web application allows users to create, update and delete tasks
+
+To run this web application locally, follow the instructions below
+
+1. Clome this repository
+```bash
+git clone {repository link}
+```
+
+2. Install dependencies
+```bash
+cd [project folder]
+npm install
+```
+
+3. Run the application
+```bash
+node app
+```
+
+### Dependencies of the web application
+- express.js
+- pug.js
+
+### Web application repository on github
+
+[url to github repo]
+
+### Explanaition to not fully following the project structure recommended in the cw description
+
+I did not add routes folder to store functions and handlers for different pages. The reason for that is that I have only 2 extra functions: 'start writing task1' and 'instructions' Both of them did not need a lot of lines code to be initiated. So in order to decrease the amount of work, I decided to add comments to all the function/handler codes. This way, I specified to which handler a certain function is related to
--- app.js
+++ app.js
@@ -12,7 +12,21 @@ app.use('/static', express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 
 // http://localhost:8000
+// this function will render intro page whenever the above link is entered in browser
 app.get('/', (req, res) => {
+  res.render("intro", { title: "Hi", message: "Welcome" })
+
+})
+
+// this function will enable instructions button and open the instructions page
+app.get('/instructions', (req, res) => {
+  res.render("instructions", { title: "info", message: "Welcome" })
+
+})
+
+
+//this function will get us the home page where we can start writing tasks
+app.get('/add', (req, res) => {
   fs.readFile('./data/tasks.json', (err, data) => {
     if (err) throw err
 
@@ -20,8 +34,8 @@ app.get('/', (req, res) => {
 
     res.render('home', { tasks: tasks })
   })
-
 })
+// this function enables the add button so that the usre input will be added to the list and displayed to the user
 app.post('/add', (req, res) => {
   const formData = req.body
 
@@ -41,7 +55,7 @@ app.post('/add', (req, res) => {
 
       const task = {
         id: id(),
-        description: formData.todo,
+        description: formData.task,
         done: false
       }
 
@@ -60,6 +74,60 @@ app.post('/add', (req, res) => {
       })
     })
   }
+})
+
+//this function enables remove button so that user can delete tasks if he/she wants to
+app.get('/:id/delete', (req,res) => {
+  const id = req.params.id
+
+  fs.readFile('./data/tasks.json', (err,data) =>{
+    if (err) throw err
+
+    const tasks = JSON.parse(data)
+
+    const filteredTasks  = tasks.filter(task => task.id != id)
+
+    fs.writeFile('./data/tasks.json',JSON.stringify(filteredTasks), (err) => {
+      if (err) throw err
+
+      res.render('home', {tasks: filteredTasks, removed: true})
+    })
+  })
+})
+// this function allows us to add erasing line to the task name when the task name is clicked
+app.get('/:id/update', (req, res) => {
+
+  const id = req.params.id
+
+  fs.readFile('./data/tasks.json', (err, data) =>{
+    if (err) throw err
+
+    const tasks = JSON.parse(data)
+    const task = tasks.filter(task => task.id == id)[0]
+
+    const taskIds = tasks.indexOf(task)
+    const splicedTask = tasks.splice(taskIds, 1)[0]
+
+    splicedTask.done = true
+
+    tasks.push(splicedTask)
+
+    fs.writeFile('./data/tasks.json',JSON.stringify(tasks), (err)  => {
+      if (err) throw err
+
+      res.render('home', {tasks: tasks})
+    })
+  })
+})
+// basic REST api
+app.get('/api/v1/tasks', (req, res) => {
+  fs.readFile('./data/tasks.json', (err, data) => {
+    if (err) throw err
+
+    const tasks = JSON.parse(data)
+
+    res.json(tasks)
+  })
 })
 
 app.listen(PORT, (err) =>{
